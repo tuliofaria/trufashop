1aeec1436c1922c3843d579ce75051e971148e09
Tulio Faria <tuliofaria@gmail.com>
2021-03-15 23:21:19 -0300

    fix

--- trufa-shop-backend/lib/pix.js
+++ trufa-shop-backend/lib/pix.js
@@ -97,7 +97,7 @@ const createPixCharge = async () => {
       nome: 'Tulio Faria',
     },
     valor: {
-      original: '0.13',
+      original: '0.14',
     },
     chave, // pelo app do gerencianet
     solicitacaoPagador: 'Cobrança dos serviços prestados',
